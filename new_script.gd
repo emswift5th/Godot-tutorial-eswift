741a3c90796f60c509d41d503f730f7b632c5aa8
emswift5th <emswifttng@gmail.com>
2023-08-20 17:43:47 +0100

    new fork, fiddling around

--- new_script.gd
+++ new_script.gd
@@ -2,13 +2,20 @@ extends Sprite2D
 
 var angular_speed = PI
 var speed =  400.0
+var toggle_direction = false
 
 func _process(delta):
 	var direction = 0
-	if Input.is_key_pressed(KEY_LEFT):
-		direction = -1
-	if Input.is_key_pressed(KEY_RIGHT):
-		direction = 1
+	if toggle_direction == false:
+		if Input.is_key_pressed(KEY_LEFT):
+			direction = -1
+		if Input.is_key_pressed(KEY_RIGHT):
+			direction = 1
+	else:
+		if Input.is_key_pressed(KEY_LEFT):
+			direction = 1
+		if Input.is_key_pressed(KEY_RIGHT):
+			direction = -1
 	
 	rotation += angular_speed * direction * delta
 	
@@ -17,3 +24,10 @@ func _process(delta):
 		velocity = Vector2.UP.rotated(rotation) * speed
 	
 	position += velocity * delta
+
+
+func _on_button_pressed():
+	if toggle_direction == true:
+		toggle_direction = false
+	else:
+		toggle_direction = true
